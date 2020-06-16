1d8ccc5fb5ff678e9d860a8721e63d9fe388e240
ashwin19971 <ashwingiri1997@gmail.com>
2020-06-16 21:11:46 +0530

    Initial Setup

---
+++ src/modules/HomePage/pages/HomePage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './HomePage.scss';
+
+class HomePage extends Component {
+
+  render() {
+    return (
+      <div id="home-page-wrapper">
+      There will be home page
+      </div>
+    )
+  }
+}
+
+export default HomePage;
